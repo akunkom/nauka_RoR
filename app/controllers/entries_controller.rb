012daee6cd52793d5f7b24ab3cb3af91111434e5
akunkom <ak3@o2.pl>
2020-01-12 14:06:13 +0100

    dodanie mozliwosci wpisu ze strony

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,4 +6,18 @@ class EntriesController < ApplicationController
   def show
     @entry = Entry.find(params["id"])
   end
+
+  def new
+  end
+
+  def create
+    @entry = Entry.create(entry_params)
+    redirect_to @entry
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :contents)
+  end
 end
