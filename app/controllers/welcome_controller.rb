eb0890b1a0ce232e29558115aa651b9ab9a1a84c
akunkom <ak3@o2.pl>
2020-01-12 13:12:06 +0100

    pokazanie jednego wpisu, linki kierujace na str. wczesniejsze

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-  @websites = [
-  ["http://railsgirls.com", "Rails Girls"],
-  ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
-  ["http://guides.rubyonrails.org", "Ruby on Rails Guides"]
-  ]
-
-  @entries = Entry.all
+    @websites = [
+      ["http://railsgirls.com", "Rails Girls"],
+      ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+      ["http://guides.rubyonrails.org", "Ruby on Rails Guides"]
+    ]
   end
 end
